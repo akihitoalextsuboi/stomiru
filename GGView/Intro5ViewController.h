76e3123d128fa86a8fcdd51adf124572a96bf1f1
rainbowaffro <akihito.tsuboi@gmail.com>
2015-08-22 13:52:06 +0900

    modify

---
+++ GGView/Intro5ViewController.h
@@ -0,0 +1,13 @@
+//
+//  Intro5ViewController.h
+//  GGView
+//
+//  Created by rainbowaffro on 2015/08/22.
+//  Copyright (c) 2015年 rainbowaffro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Intro5ViewController : UIViewController
+
+@end
